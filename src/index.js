767bc451eca6d5d7e45edf9229e075f43ffbb2b9
testing92260 <123500964+testing92260@users.noreply.github.com>
2023-01-25 00:07:00 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.scss";
+import React from "react";
+import ReactDOM from "react-dom";
+// import { createEditor } from "./editor";
+
+const element = <p>This is Awais Sajid</p>;
+// const rootElement = document.getElementById("root");
+ReactDOM.render(element, document.getElementById("root"));
+
+console.log(element);
